e9520300cf730fd7b0392cf89f5ee0d771e25fbd
Chaoran Wang <showcy@vt.edu>
2022-03-03 12:24:14 -0800

    Replace month slider by date picker.

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import altair as alt
 from altair import datum
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
+from datetime import date
 
 alt.data_transformers.disable_max_rows()
 
@@ -14,102 +15,129 @@ trees["BLOOM_END"] = pd.to_datetime(trees["BLOOM_END"])
 # Build Front End
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-app.layout = html.Div([
-    dbc.Container(
-        html.H1('Vancouver cherry blossom tracker'),
-        style={
-            'color': 'white', 
-            'background-color': 'orchid',
-            'font-family': 'Helvetica'
-        }
-    ),
-    dbc.Container(
-        dbc.Row([
-            dbc.Col([
-                html.Label(['Month'], style={'font-weight': 'bold'}),
-                dcc.RangeSlider(
-                    id="slider_month",
-                    min=0,
-                    max=5,
-                    step=1,
-                    value=[0, 5],
-                    marks={
-                        0: 'NA',
-                        1: 'Jan',
-                        2: 'Feb',
-                        3: 'Mar',
-                        4: 'Apr',
-                        5: 'May'
-                    }
-                )
-            ], width=4),
-            dbc.Col([
-                html.Label(['Neighbourhood'], style={'font-weight': 'bold'}),
-                dcc.Dropdown(
-                    id="filter_neighbourhood",
-                    value="all_neighbourhoods",
-                    options=[
-                        {'label': 'All neighbourhoods', 'value': 'all_neighbourhoods'}
-                    ] + [
-                        {"label": i, "value": i} for i in trees.NEIGHBOURHOOD_NAME.unique()
-                    ],
-                )
-            ], width=3),
-            dbc.Col([
-                html.Label(['Cherry tree cultivars'], style={'font-weight': 'bold'}),
-                dcc.Dropdown(
-                    id="filter_cultivar",
-                    value="all_cultivars",
-                    options=[
-                        {'label': 'All cultivars', 'value': 'all_cultivars'}
-                    ] + [
-                        {"label": i, "value": i} for i in trees.CULTIVAR_NAME.unique()
-                    ],
-                )
-            ], width=3),
-            dbc.Col([
-                html.Label(['Cherry tree diameter'], style={'font-weight': 'bold'}),
-                dcc.RangeSlider(
-                    id="slider_diameter",
-                    min=0,
-                    max=150,
-                    value=[0, 150],
-                    marks={
-                        0: '0cm',
-                        150: '150cm'
-                    },
-                    tooltip={
-                        "placement": "bottom", 
-                        "always_visible": True
-                    }
-                )
-            ]),
-        ]),
-        style={'background-color': 'whitesmoke'}
-    ),
-    dbc.Row([
-        dbc.Col([
-            html.Iframe(
-                id="bar", style={"border-width": "0", "width": "100%", "height": "800px"}
-            )
-        ]),
-        dbc.Col([
-            html.Iframe(
-                id="timeline", style={"border-width": "0", "width": "100%", "height": "100%"}
-            )
-        ]),
-    ])
-])
+app.layout = html.Div(
+    [
+        dbc.Container(
+            html.H1("Vancouver cherry blossom tracker"),
+            style={
+                "color": "white",
+                "background-color": "orchid",
+                "font-family": "Helvetica",
+            },
+        ),
+        dbc.Container(
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            dcc.DatePickerRange(
+                                id="picker_date",
+                                min_date_allowed=date(2022, 1, 1),
+                                max_date_allowed=date(2022, 5, 30),
+                                start_date_placeholder_text="Start Period",
+                                end_date_placeholder_text="End Period",
+                            ),
+                        ],
+                        width=4,
+                    ),
+                    dbc.Col(
+                        [
+                            html.Label(
+                                ["Neighbourhood"], style={"font-weight": "bold"}
+                            ),
+                            dcc.Dropdown(
+                                id="filter_neighbourhood",
+                                value="all_neighbourhoods",
+                                options=[
+                                    {
+                                        "label": "All neighbourhoods",
+                                        "value": "all_neighbourhoods",
+                                    }
+                                ]
+                                + [
+                                    {"label": i, "value": i}
+                                    for i in trees.NEIGHBOURHOOD_NAME.unique()
+                                ],
+                            ),
+                        ],
+                        width=3,
+                    ),
+                    dbc.Col(
+                        [
+                            html.Label(
+                                ["Cherry tree cultivars"], style={"font-weight": "bold"}
+                            ),
+                            dcc.Dropdown(
+                                id="filter_cultivar",
+                                value="all_cultivars",
+                                options=[
+                                    {"label": "All cultivars", "value": "all_cultivars"}
+                                ]
+                                + [
+                                    {"label": i, "value": i}
+                                    for i in trees.CULTIVAR_NAME.unique()
+                                ],
+                            ),
+                        ],
+                        width=3,
+                    ),
+                    dbc.Col(
+                        [
+                            html.Label(
+                                ["Cherry tree diameter"], style={"font-weight": "bold"}
+                            ),
+                            dcc.RangeSlider(
+                                id="slider_diameter",
+                                min=0,
+                                max=150,
+                                value=[0, 150],
+                                marks={0: "0cm", 150: "150cm"},
+                                tooltip={"placement": "bottom", "always_visible": True},
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            style={"background-color": "whitesmoke"},
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Iframe(
+                            id="bar",
+                            style={
+                                "border-width": "0",
+                                "width": "100%",
+                                "height": "800px",
+                            },
+                        )
+                    ]
+                ),
+                dbc.Col(
+                    [
+                        html.Iframe(
+                            id="timeline",
+                            style={
+                                "border-width": "0",
+                                "width": "100%",
+                                "height": "100%",
+                            },
+                        )
+                    ]
+                ),
+            ]
+        ),
+    ]
+)
 
 
-@app.callback(
-    Output("bar", "srcDoc"), 
-    Input("filter_neighbourhood", "value"))
+@app.callback(Output("bar", "srcDoc"), Input("filter_neighbourhood", "value"))
 ##Create Cultivar Chart
 def bar_plot(neighbourhood):
 
-    if neighbourhood != 'all_neighbourhoods':
-        trees_bar = trees[trees['NEIGHBOURHOOD_NAME'] == neighbourhood]
+    if neighbourhood != "all_neighbourhoods":
+        trees_bar = trees[trees["NEIGHBOURHOOD_NAME"] == neighbourhood]
     else:
         trees_bar = trees
 
@@ -133,46 +161,82 @@ def bar_plot(neighbourhood):
 
     return bar_plot.to_html()
 
+
 @app.callback(
     Output("timeline", "srcDoc"),
-    Input("slider_month", "value"),
+    Input("picker_date", "start_date"),
+    Input("picker_date", "end_date"),
     Input("filter_neighbourhood", "value"),
     Input("filter_cultivar", "value"),
-    Input("slider_diameter", "value"))
+    Input("slider_diameter", "value"),
+)
 ##Create Cultivar Chart
-def timeline(month_range, neighbourhood, cultivar, diameter_range):
+def timeline(start_date, end_date, neighbourhood, cultivar, diameter_range):
     trees_timeline = trees.dropna(subset=["BLOOM_START", "BLOOM_END"])
 
+    if start_date is None:
+        start_date = "2022-01-01"
+
+    if end_date is None:
+        end_date = "2022-05-30"
+
+    start_date = pd.Timestamp(date.fromisoformat(start_date))
+    end_date = pd.Timestamp(date.fromisoformat(end_date))
+
     trees_timeline = trees_timeline[
-        (trees_timeline['BLOOM_START_MONTH']>=month_range[0]) 
-        & (trees_timeline['BLOOM_END_MONTH']<=month_range[1])]
+        (
+            (trees_timeline["BLOOM_START"] <= start_date)
+            & (trees_timeline["BLOOM_END"] >= start_date)
+        )
+        | (
+            (trees_timeline["BLOOM_START"] <= end_date)
+            & (trees_timeline["BLOOM_END"] >= end_date)
+        )
+        | (trees_timeline["BLOOM_START"].between(start_date, end_date))
+        | (trees_timeline["BLOOM_END"].between(start_date, end_date))
+    ]
 
-    trees_timeline = trees_timeline[trees_timeline['DIAMETER'].between(diameter_range[0], diameter_range[1])]
+    trees_timeline = trees_timeline[
+        trees_timeline["DIAMETER"].between(diameter_range[0], diameter_range[1])
+    ]
 
-    if neighbourhood != 'all_neighbourhoods':
-        trees_timeline = trees_timeline[trees_timeline['NEIGHBOURHOOD_NAME'] == neighbourhood]
-
-    if cultivar != 'all_cultivars':
-        trees_timeline = trees_timeline[trees_timeline['CULTIVAR_NAME'] == cultivar]
-
-    timeline = alt.Chart(trees_timeline).mark_bar().encode(
-        x=alt.X('BLOOM_START', axis=alt.Axis(
-            values=[d.isoformat() for d in pd.date_range(start='2022-01-01', end='2022-5-31', freq='1M')],
-            format="%b",
-            tickCount=5,
-            orient='top'), title=None),
-        x2='BLOOM_END',
-        y=alt.Y('CULTIVAR_NAME:N', title=None),
-        tooltip=[
-            alt.Tooltip('BLOOM_START', title='Start'), 
-            alt.Tooltip('BLOOM_END', title='End')
+    if neighbourhood != "all_neighbourhoods":
+        trees_timeline = trees_timeline[
+            trees_timeline["NEIGHBOURHOOD_NAME"] == neighbourhood
         ]
-    ).configure_mark(
-        color="pink"
-    ).configure_axis(
-        domainOpacity=0
-    ).configure_view(
-        strokeOpacity=0
+
+    if cultivar != "all_cultivars":
+        trees_timeline = trees_timeline[trees_timeline["CULTIVAR_NAME"] == cultivar]
+
+    timeline = (
+        alt.Chart(trees_timeline)
+        .mark_bar()
+        .encode(
+            x=alt.X(
+                "BLOOM_START",
+                axis=alt.Axis(
+                    values=[
+                        d.isoformat()
+                        for d in pd.date_range(
+                            start="2022-01-01", end="2022-5-31", freq="1M"
+                        )
+                    ],
+                    format="%b",
+                    tickCount=5,
+                    orient="top",
+                ),
+                title=None,
+            ),
+            x2="BLOOM_END",
+            y=alt.Y("CULTIVAR_NAME:N", title=None),
+            tooltip=[
+                alt.Tooltip("BLOOM_START", title="Start"),
+                alt.Tooltip("BLOOM_END", title="End"),
+            ],
+        )
+        .configure_mark(color="pink")
+        .configure_axis(domainOpacity=0)
+        .configure_view(strokeOpacity=0)
     )
 
     return timeline.to_html()
