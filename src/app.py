f23028ae0afb026252d592358bb18259a8e9b9eb
Gabe Fairbrother <gfairbrother@gmail.com>
2022-03-04 12:18:05 -0800

    Remove Filter

--- src/app.py
+++ src/app.py
@@ -335,7 +335,6 @@ def bar_plot(trees_bar):
             tooltip=alt.Tooltip("count:Q"),
         )
         .transform_aggregate(count="count()", groupby=["CULTIVAR_NAME"])
-        .transform_filter("datum.count > 10")
         .configure_mark(opacity=0.6, color="#F3B2D2")
         .interactive()
     )
